8c0a1567a76ec04239ce9c69e9dc2bc6289452ab
RhythmSyed <msr915@gmail.com>
2021-01-12 16:01:51 -0600

    added coref notebook, neuralcoref, and optimized output graph

--- text_to_graph.py
+++ text_to_graph.py
@@ -1,14 +1,14 @@
 import spacy
+import string
 import pandas as pd
-import en_core_web_lg
 from stanfordcorenlp import StanfordCoreNLP
 import json
 from collections import defaultdict
 import nltk
-import sys
 from docopt import docopt
 from openie import StanfordOpenIE
 from sentence_transformers import SentenceTransformer, util
+import neuralcoref
 
 
 def process_NER(text):
@@ -20,9 +20,9 @@ def process_NER(text):
     return ner_dict
 
 
-def process_corefs(text):
-    nlp = StanfordCoreNLP("./stanford-corenlp-4.2.0", quiet=False)
-    annotated = nlp.annotate(text, properties={'annotators': 'coref', 'pipelineLanguage': 'en'})
+def process_corefs(text, corenlp_path):
+    nlp = StanfordCoreNLP(corenlp_path, quiet=False)
+    annotated = nlp.annotate(text, properties={'annotators': 'coref', 'pipelineLanguage': 'en', 'coref.algorithm' : 'statistical'})
     result = json.loads(annotated)
     corefs = result['corefs']
     return corefs
@@ -31,6 +31,10 @@ def process_corefs(text):
 def process_dependency_matching(text, ner_dict, corefs):
     replace_coref_with = []
     sentence_wise_replacements = defaultdict(list)
+    # nlp = spacy.load('en')
+    # doc = nlp(text)
+    # sentences = [sent.string.strip() for sent in doc.sents]
+
     sentences = nltk.sent_tokenize(text)
 
     for index, coreferences in enumerate(corefs.values()):
@@ -49,15 +53,34 @@ def process_dependency_matching(text, ner_dict, corefs):
             to_replace = item[0]
             replace_with = replace_coref_with[item[1]]
             replaced_sent = ""
+            # doc = nlp(sent)
+            # words = [token.text for token in doc]
             words = nltk.word_tokenize(sent)
 
+            punctuation_flag = False
             for i in range(len(words) - 1, to_replace["endIndex"] - 2, -1):
-                replaced_sent = words[i] + " " + replaced_sent
+                if words[i] in string.punctuation:
+                    replaced_sent = words[i] + ' ' + replaced_sent
+                    punctuation_flag = True
+                else:
+                    if punctuation_flag:
+                        replaced_sent = words[i] + replaced_sent
+                    else:
+                        replaced_sent = words[i] + ' ' + replaced_sent
+                    punctuation_flag = False
 
             replaced_sent = replace_with + " " + replaced_sent
 
             for i in range(to_replace["startIndex"] - 2, -1, -1):
-                replaced_sent = words[i] + " " + replaced_sent
+                if words[i] in string.punctuation:
+                    replaced_sent = words[i] + ' ' + replaced_sent
+                    punctuation_flag = True
+                else:
+                    if punctuation_flag:
+                        replaced_sent = words[i] + replaced_sent
+                    else:
+                        replaced_sent = words[i] + ' ' + replaced_sent
+                    punctuation_flag = False
 
             # print(replaced_sent)
             sentences[index] = replaced_sent
@@ -65,6 +88,13 @@ def process_dependency_matching(text, ner_dict, corefs):
     for sent in sentences:
         result += sent
     return result
+
+
+def process_neural_coref(text):
+    nlp = spacy.load('en')
+    neuralcoref.add_to_pipe(nlp)
+    doc = nlp(text)
+    return doc._.coref_resolved
 
 
 def process_relation_extraction(text):
@@ -84,14 +114,14 @@ def process_triple_pruning(triples, ner_dict):
         col['subject'] = col['subject'].strip()
 
         # check if Named Entity in subject sentence fragment
-        found_entity = False
-        for named_entity in entity_set:
-            if named_entity in col['subject']:
-                col['subject'] = named_entity
-                found_entity = True
-
-        if found_entity:
-            final_triples.append(('Node', col['subject'], col['relation'], 'Node', col['object']))
+        # found_entity = False
+        # for named_entity in entity_set:
+        #     if named_entity in col['subject']:
+        #         col['subject'] = named_entity
+        #         found_entity = True
+        #
+        # if found_entity:
+        final_triples.append(('Node', col['subject'], col['relation'], 'Node', col['object']))
 
     triple_df = pd.DataFrame(final_triples, columns=['Type1', 'Entity1', 'Relationship', 'Type2', 'Entity2']).drop_duplicates()
     return triple_df
@@ -99,14 +129,25 @@ def process_triple_pruning(triples, ner_dict):
 
 def process_entity_linking(triple_df, confidence_threshold):
     model = SentenceTransformer('paraphrase-distilroberta-base-v1')
+    triple_df = linker(triple_df=triple_df, head_name='Entity2', tail_name='Entity2', model=model,
+                       confidence_threshold=confidence_threshold)
+    triple_df = linker(triple_df=triple_df, head_name='Entity1', tail_name='Entity1', model=model,
+                       confidence_threshold=confidence_threshold)
+    triple_df = linker(triple_df=triple_df, head_name='Entity1', tail_name='Entity2', model=model,
+                       confidence_threshold=confidence_threshold)
 
-    index = 0
+    triple_df = triple_df.drop_duplicates()
+    return triple_df
+
+
+def linker(triple_df, head_name, tail_name, model, confidence_threshold):
+    index = 1
     for _, col1 in triple_df.iterrows():
-        head = col1['Entity2']
+        head = col1[head_name]
         embedding1 = model.encode(head, convert_to_tensor=True)
 
         for _, col2 in triple_df.iterrows():
-            tail = col2['Entity2']
+            tail = col2[tail_name]
             if head == tail:
                 continue
 
@@ -115,19 +156,17 @@ def process_entity_linking(triple_df, confidence_threshold):
 
             if confidence > confidence_threshold:  # 85% seems to work pretty well
                 # Perform logic for linking
-                new_tail = head if len(tail) < len(head) else tail
+                new_tail = tail if len(tail) < len(head) else head
 
-                col1['Entity2'] = new_tail
-                col2['Entity2'] = new_tail
+                col1[head_name] = new_tail
+                col2[tail_name] = new_tail
 
                 print("Sentence 1:", head)
                 print("Sentence 2:", tail)
                 print("Similarity:", confidence)
+                print('Processed {}\n'.format(index))
                 index += 1
-                print('Processed {} out of {}'.format(index, len(triple_df)))
-                print()
 
-    triple_df = triple_df.drop_duplicates()
     return triple_df
 
 
@@ -154,13 +193,17 @@ def main():
     print('***** Completed NER *****')
 
     # Generate Coreferences and Dependencies with CoreNLP
-    corefs = process_corefs(text=input_text)
+    corefs = process_corefs(text=input_text, corenlp_path='./stanford-corenlp-4.2.0')
     print("Coreferences found: ", len(corefs))
     print("Named entities: ", ner_dict.keys())
 
     # Perform Replacement with Named Entities and Dependencies
     resolved_text = process_dependency_matching(text=input_text, ner_dict=ner_dict, corefs=corefs)
     print('***** Completed Coreference Resolution *****')
+
+    # Add Neural Coref
+    resolved_text = process_neural_coref(resolved_text)
+    print('***** Completed Coreference Resolution with NeuralCoref *****')
 
     # Perform Relation Extraction using Stanford OpenIE
     triples = process_relation_extraction(text=resolved_text)
@@ -171,11 +214,11 @@ def main():
     print('***** Completed Pruning *****')
 
     # Perform Entity Linking between nodes
-    triple_df = process_entity_linking(triple_df=triple_df, confidence_threshold=0.70)
+    triple_df = process_entity_linking(triple_df=triple_df, confidence_threshold=0.75)
     print('***** Completed Entity Linking *****')
 
     # Write to csv
-    triple_df.to_csv('output_processed.csv', encoding='utf-8', index=False)
+    triple_df.to_csv('output_processed_neuralcoref_corenlp.csv', encoding='utf-8', index=False)
 
 
 if __name__ == '__main__':
